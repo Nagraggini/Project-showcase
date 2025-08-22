34bcf1bd652ce2478c2b60e3d2dd4db56f954938
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-08-22 20:41:47 +0200

    Fixing first Azure project.

--- Backend_projects/src/1-Azure_SWA/The_weather_in_London/main_The_weather_in_London.js
+++ Backend_projects/src/1-Azure_SWA/The_weather_in_London/main_The_weather_in_London.js
@@ -1,12 +1,20 @@
 //Kikeresi a HTML-ből azt az elemet, amelynek az id-je "weather" (jelen esetben span), és eltárolja a weather változóban.
 //Ebbe fogjuk kiírni az időjárási adatot.
-const weather = document.getElementById("weather");
+const Budapest_weather = document.getElementById("Budapest_weather");
+const London_weather = document.getElementById("London_weather");
+const Tokyo_weather = document.getElementById("Tokyo_weather");
 
 //Ugyanez, csak a "btn" id-jű elemet (pl. egy gombot) menti el a btn változóba.
 const btn = document.getElementById("btn");
 
 //Létrehoz egy URL-t az Open-Meteo API-hoz, ami London (51.5074, -0.1278) aktuális időjárási adatait fogja visszaadni.
-let url =
+let Budapest_url =
+  "https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current_weather=true";
+
+let London_url =
+  "https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current_weather=true";
+
+let Tokyo_url =
   "https://api.open-meteo.com/v1/forecast?latitude=51.5074&longitude=-0.1278&current_weather=true";
 
 //Lekéri az adatokat az API-tól (fetch(url)).
@@ -17,7 +25,22 @@ let url =
 fetch(url)
   .then((response) => response.json())
   .then(
-    (data) => (weather.textContent = data.current_weather.temperature + "°C")
+    (data) =>
+      (Budapest_weather.textContent = data.current_weather.temperature + "°C")
+  );
+
+fetch(url)
+  .then((response) => response.json())
+  .then(
+    (data) =>
+      (London_weather.textContent = data.current_weather.temperature + "°C")
+  );
+
+fetch(url)
+  .then((response) => response.json())
+  .then(
+    (data) =>
+      (Budapest_weather.textContent = data.current_weather.temperature + "°C")
   );
 
 //Ez egy függvény, ami akkor fut le, ha a gombot megnyomod.
