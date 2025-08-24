15807101494f7d39c98b942dcca5fd75843de920
Andrea Freud <62719085+Nagraggini@users.noreply.github.com>
2025-08-24 10:26:09 +0200

    Delete 0-Connect_Python_to_HTML

--- Frontend_projects/src/Html+css+python_projects/Python_websites/0-Connect_Python_to_HTML/main.py
+++
@@ -1,7 +0,0 @@
-from pyscript import document
-
-print("Hello World, From The Web")  # Láthatod a konzolon a szöveget.
-
-output_div = document.querySelector("#textarea")
-
-output_div.innerText = "Hello World, From The Web"  # You can see on website
